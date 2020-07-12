e1c145a9b5e9f732004d11d4c1be5ead979b2508
Muhammad Fuad Ardiono <fuad@investree.id>
2020-07-13 00:01:18 +0800

    Fix middleware

--- routes/module/api/AnnouncementRoute.php
+++ routes/module/api/AnnouncementRoute.php
@@ -1,8 +1,8 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => 'announcement', 'middleware' => 'jwt_guard'], function () {
-	Route::group(['middleware' => 'acl:admin'], function () {
+Route::group(['prefix' => 'announcement',], function () {
+	Route::group(['middleware' => ['acl:admin', 'jwt_guard']], function () {
 		Route::post('store', 'Api\AnnouncementController@store');
 		Route::put('mark-not-active/{id}', 'Api\AnnouncementController@markNotActive');
 	});
