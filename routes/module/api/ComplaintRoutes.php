03fbf22797b0b3a5029d245f3410ee07bf221fcf
Muhammad Fuad Ardiono <fuad@investree.id>
2020-09-12 20:03:47 +0800

    Fix ACL complaint routes

--- routes/module/api/ComplaintRoutes.php
+++ routes/module/api/ComplaintRoutes.php
@@ -5,9 +5,8 @@ Route::group(['prefix' => 'complaint', 'middleware' => 'jwt_guard'], function ()
 	Route::group(['middleware' => 'acl:resident'], function () {
 		Route::post('store', 'Api\ComplaintController@store');
 	});
-
+	Route::get('/', 'Api\ComplaintController@index');
 	Route::group(['middleware' => 'acl:admin'], function() {
-		Route::get('/', 'Api\ComplaintController@index');
 		Route::put('mark-is-read/{id}', 'Api\ComplaintController@markIsRead');
 		Route::put('mark-is-pending/{id}', 'Api\ComplaintController@markIsPending');
 		Route::put('mark-is-acc/{id}', 'Api\ComplaintController@markIsAcc');
