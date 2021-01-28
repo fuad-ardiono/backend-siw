39ea99bf4194b11e5436cef1123850132e43bb3c
Fuad Ardiono <fuad@investree.id>
2021-01-28 23:34:36 +0800

    Test commit

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/belajar-1', function() {
+	return view('module1.welcome2');
+});
